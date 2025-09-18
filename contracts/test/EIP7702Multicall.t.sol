7ef923e464f1d337dd985a21d6a91f2be61cf591
Taras Nurko <tarasnurko84@gmail.com>
2025-09-18 09:52:34 +0300

    feat: add tests to check who is msg.sender and tx.origin when delegatee cals other contract

---
+++ contracts/test/EIP7702Multicall.t.sol
@@ -0,0 +1,108 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Utils} from "../src/Utils.sol";
+import {Multicall} from "../src/Multicall.sol";
+import {EIP7702TestSetup} from "./EIP7702TestSetup.t.sol";
+import {EIP7702Utils} from "@openzeppelin/contracts/account/utils/EIP7702Utils.sol";
+
+contract EIP7702MulticallTest is EIP7702TestSetup {
+    Utils public utils;
+    Multicall public multicall;
+
+    function _assertMulticallDelegatedToEOA(address eoa) private view {
+        _assertDelegatedTo(eoa, address(multicall));
+    }
+
+    function setUp() public {
+        utils = new Utils();
+        multicall = new Multicall();
+    }
+
+    function test_eip7702_msgSenderIsDelegatedEoAWhenMulticallCallsUtils()
+        public
+    {
+        // when delegatee (contract) calls other contract, msg.sender would be delegator (EoA)
+
+        vm.signAndAttachDelegation(address(multicall), ALICE_PK);
+        _assertMulticallDelegatedToEOA(alice);
+
+        address[] memory targets = new address[](1);
+        bytes[] memory data = new bytes[](1);
+        targets[0] = address(utils);
+        data[0] = abi.encodeWithSignature("isSender(address)", alice);
+
+        vm.prank(alice, alice);
+        Multicall(address(alice)).multicall(targets, data);
+    }
+
+    function test_eip7702_txOriginIsCallerWhenMulticallCallsUtils() public {
+        // tx.origin is always EoA
+
+        vm.signAndAttachDelegation(address(multicall), ALICE_PK);
+        _assertMulticallDelegatedToEOA(alice);
+
+        address[] memory targets = new address[](1);
+        bytes[] memory data = new bytes[](1);
+        targets[0] = address(utils);
+        data[0] = abi.encodeWithSignature("isTxOrigin(address)", alice);
+
+        vm.prank(alice, alice);
+        Multicall(address(alice)).multicall(targets, data);
+    }
+
+    function test_eip7702_thirdPartyCallsDelegatedEoAMulticall() public {
+        // when other actor (Bob) (EoA) calls delegator (Alice) (EoA) that have delegated Contract, msg.sender when delegated Contract calls other contract would be the Delegator (Alice) (EoA)
+
+        vm.signAndAttachDelegation(address(multicall), ALICE_PK);
+
+        address[] memory targets = new address[](1);
+        bytes[] memory data = new bytes[](1);
+        targets[0] = address(utils);
+        data[0] = abi.encodeWithSignature("isSender(address)", alice);
+
+        vm.prank(bob, bob);
+        Multicall(address(alice)).multicall(targets, data);
+    }
+
+    function test_eip7702_multicallBatchUtilsCalls() public {
+        vm.signAndAttachDelegation(address(multicall), ALICE_PK);
+
+        address[] memory targets = new address[](2);
+        bytes[] memory data = new bytes[](2);
+        targets[0] = address(utils);
+        targets[1] = address(utils);
+        data[0] = abi.encodeWithSignature("isSender(address)", alice);
+        data[1] = abi.encodeWithSignature("isTxOrigin(address)", alice);
+
+        vm.prank(alice, alice);
+        Multicall(address(alice)).multicall(targets, data);
+    }
+
+    function test_eip7702_multicallUtilsFailsWithWrongSender() public {
+        vm.signAndAttachDelegation(address(multicall), ALICE_PK);
+
+        address[] memory targets = new address[](1);
+        bytes[] memory data = new bytes[](1);
+        targets[0] = address(utils);
+        data[0] = abi.encodeWithSignature("isSender(address)", bob);
+
+        vm.prank(alice, alice);
+        vm.expectRevert("Not msg.sender");
+        Multicall(address(alice)).multicall(targets, data);
+    }
+
+    function test_eip7702_thirdPartyTxOriginThroughMulticall() public {
+        // tx.origin is always EoA that initialized transaction
+
+        vm.signAndAttachDelegation(address(multicall), ALICE_PK);
+
+        address[] memory targets = new address[](1);
+        bytes[] memory data = new bytes[](1);
+        targets[0] = address(utils);
+        data[0] = abi.encodeWithSignature("isTxOrigin(address)", bob);
+
+        vm.prank(bob, bob);
+        Multicall(address(alice)).multicall(targets, data);
+    }
+}
